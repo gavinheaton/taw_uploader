591168fa7fd820bb55f7e5c17be174a2215fe9ff
Gavin Heaton <gavin@disruptorsco.com>
2021-09-14 21:17:27 +1000

    Update taw-uploader.php
    Working on front end

--- taw-uploader.php
+++ taw-uploader.php
@@ -18,11 +18,59 @@ function taw_uploader_setup_menu(){
     add_menu_page( 'TAW Uploader', 'TAW Uploader', 'manage_options', 'taw-uploader', 'taw_uploader_init' );
 }
 
-function listFiles($dir2){
-  $fulldir = "/wp-content/uploads/uploader/";
-  echo "<table><tr><th>File name</th><th>Image</th></tr>";
+add_shortcode( 'taw_upload_shortcode', 'taw_upload_shortcode' );
 
-  $files = glob($dir2 . "*.*");
+//function taw_uploader_init(){
+// Because we are working on the front end we use the short code
+function taw_upload_shortcode(){
+  // Get the slug from the url
+  global $post;
+  $post_slug = $post->post_name;
+  $upload_dir = wp_upload_dir();
+
+  // check if the upload directory related to this page is available
+  $uploadCheck = $upload_dir['basedir'] . '/uploader/' . $post_slug.'/';
+  //echo "<p>Posting to {$uploadCheck}</p>";
+  if ( ! file_exists( $uploadCheck ) ) {
+          wp_mkdir_p( $uploadCheck );
+        }
+  //$upload_dir2 = wp_upload_dir();
+  //$dir2 = $upload_dir2["basedir"]."/uploader/";
+  global $dir2;
+  $dir2 = $uploadCheck;
+  taw_upload();
+?>
+  <h3>Upload a File</h3>
+  <?php
+  listFiles($dir2);
+  ?>
+  <!-- Form to handle the upload - The enctype value here is very important -->
+  <form  method="post" enctype="multipart/form-data">
+      <input type='file' id='taw_upload_file' name='taw_upload_file'></input>
+      <br />
+      <!--?php submit_button('Upload') ?> -->
+      <input type='submit'>
+  </form>
+  <br />
+<?php
+//echo "Displaying {$dir2}";
+//getFileList($dir2);
+//echo "-----";
+}
+
+function listFiles($dir2){
+  global $post;
+  $post_slug = $post->post_name;
+  $upload_dir = wp_upload_dir();
+
+  // check if the upload directory related to this page is available
+  $uploadCheck = $upload_dir['basedir'] . '/uploader/' . $post_slug.'/';
+  //echo "<p>Uploading to {$uploadCheck}</p>";
+
+  //$fulldir = "/wp-content/uploads/uploader/";
+  echo "<table><tr><th>File name</th><th>Actions</th></tr>";
+
+  $files = glob($uploadCheck . "*.*");
   for ($i = 0; $i < count($files); $i++) {
     $image = $files[$i];
     $supported_file = array(
@@ -36,7 +84,7 @@ function listFiles($dir2){
     $ext = strtolower(pathinfo($image, PATHINFO_EXTENSION));
     if (in_array($ext, $supported_file)) {
       echo "<tr><td>".basename($image)."</td>";
-      echo '<td><img src="' . $fulldir . '/' . basename($image) . '" alt="Random image" ,width=50px, height=50px /></td></tr>';
+      echo '<td><a target="_blank" href="' . $uploadCheck . basename($image) . '">Download</a></td></tr>';
     } else {
       continue;
     }
@@ -44,47 +92,15 @@ function listFiles($dir2){
   echo "</table>";
 }
 
-function getFileList($dir2)
-{
-  // NO LONGER USED
-  // array to hold return value
-  echo "<h3>Files</h3>";
-
-  // open pointer to directory and read list of files
-  $d = dir($dir2);
-  //echo "Path: " . $d->path . "<br>";
-  // Setup table
-  echo "<table><tr><th>File name</th></tr>";
-  while (($file = $d->read()) !== false){
-    echo "<tr><td>" . $file . "</td></tr>";
-  }
-  $d->close();
-  echo "</table>";
-}
-
-function taw_uploader_init(){
-  $upload_dir2 = wp_upload_dir();
-  $dir2 = $upload_dir2["basedir"]."/uploader/";
-  taw_upload();
-?>
-
-<h1>TheAir.Works Uploader</h1>
-  <h2>Upload a File</h2>
-  <!-- Form to handle the upload - The enctype value here is very important -->
-  <!-- Form to handle the upload - The enctype value here is very important -->
-  <form  method="post" enctype="multipart/form-data">
-      <input type='file' id='taw_upload_file' name='taw_upload_file'></input>
-      <?php submit_button('Upload') ?>
-  </form>
-<?php
-//echo "Displaying {$dir2}";
-//getFileList($dir2);
-//echo "-----";
-listFiles($dir2);
-}
-
 function taw_upload(){
+  global $post;
+  $post_slug = $post->post_name;
   $upload_dir = wp_upload_dir();
+
+  // check if the upload directory related to this page is available
+  $uploadCheck = $upload_dir['basedir'] . '/uploader/' . $post_slug.'/';
+  $dir2 = $uploadCheck;
+
   //echo "<p>Base directory is ". $upload_dir['basedir'] . "</p>";
   //$upload_dir = "/wp-content/uploads/uploader";
   //CREATE DIRECTORY
@@ -92,29 +108,17 @@ function taw_upload(){
       $pdf = $_FILES['taw_upload_file'];
     //print_r($pdf);
     //
-    $user_dirname = $upload_dir['basedir'].'/uploader/';
+    $user_dirname = $dir2;
+    //echo "<p>The user directory is {$dir2}";
+
             if ( ! file_exists( $user_dirname ) ) {
             wp_mkdir_p( $user_dirname );
         }
     //echo "<p>Uploading now to {$user_dirname}</p>";
 
-
-    if (!is_file($pdf) && !is_dir($pdf)) {
-      //mkdir($dir); //create the directory
-      //wp_mkdir_p($targetfilename);
-      //echo "<p>{$pdf} does not exist.</p>";
-  }
-  else
-  {
-      //echo "{$pdf} exists and is a valid dir";
-  }
-  $dir = $user_dirname;
-  $a = scandir($dir);
-  $b = scandir($dir,1);
-  //print_r($a);
-  //print_r($b);
   // upload
   $target_dir = $user_dirname;
+  //echo "<p>The target directory is {$target_dir}";
   $target_file = $target_dir . basename($_FILES['taw_upload_file']["name"]);
   //echo "<div><p>Uploading {$target_file}</p></div>";
   //print_r($target_file);
